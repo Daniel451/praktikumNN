4f09568674623232a1d468dd1b7e579bec9d97a0
Daniel Speck <daniel@dspeck.de>
2014-11-03 16:04:00 +0100

    einlesewerkzeug für ziffern/zahlen

--- PyMLP/AlphaNum.py
+++ PyMLP/AlphaNum.py
@@ -22,3 +22,35 @@ for i in [[0, 0], [0, 1], [1, 0], [1,1]]:
 #nn.save('savetest') # erzeugt eine 'savetest.npz' Datei! (alles unchecked!, überschreiben ohne Warnung!)
     
 #nn.load('savetest') # läd eine 'savetest.npz' Datei! (alles unchecked!)
+
+
+
+
+
+class world_digits:
+"""reads the 8x8 image files in digits_alph that display digits and capital letters"""
+
+   def __init__(self):
+      self.digitsalph = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      self.values = numpy.zeros((self.len(digitsalph), 8*8))
+      for tt in range(len(digitsalph)):
+         filename = "digits_alph/digit" + self.digitsalph[tt] + ".pgm"
+         self.values[tt], h, w = KT.importimage (filename)
+         if  h != 8 or w != 8:
+            print "digits_alph files don't match expected size!"
+      self.datumsize = h*w
+      self.seqlen = len(self.digitsalph)
+
+   def dim(self):
+      return (self.datumsize, self.seqlen)
+
+   def newinit(self):
+      self.t = 0
+
+   def act(self):
+      # world reaction
+      self.t += 1
+
+   def sensor(self):
+      # return one image as an 8x8 long vector
+      return [self.values[self.t], self.digitsalph[self.t]]
