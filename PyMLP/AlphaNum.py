93f73a7c7287c4b5b1d9335bde3cecb3e907eb3f
Florian Kock <florian-kock@gmx.de>
2014-11-03 17:17:08 +0100

    weiter gehts...

--- PyMLP/AlphaNum.py
+++ PyMLP/AlphaNum.py
@@ -9,6 +9,7 @@ class world_digits:
     """reads the 8x8 image files in digits_alph that display digits and capital letters"""
 
     def __init__(self):
+        self.t = 0
         self.digitsalph = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         self.values = numpy.zeros((len(self.digitsalph), 8*8))
         for tt in range(len(self.digitsalph)):
@@ -34,51 +35,58 @@ class world_digits:
         # [0] -> one image as an 8x8 long vector
         # [1] -> alphanumerical thing
         return [self.values[self.t], self.digitsalph[self.t]]
+        
+    def getall(self):
+        return [self.values, list(self.digitsalph)]
+        
 
 
 
 
+s_teach =[]
 
-s_in =n.array([])
-s_teach =n.array([])
-for file in glob.glob("./digits_alph/*.pgm"):
-    
-    char = file[file.rfind("/")+6:-4]
-    path = file
-    code = ord(char) - 48
-    binary = list(("{0:b}".format(code)).zfill(6))
-    binary = [int(i) for i in binary]
-    print(char,path, code, str(binary))
-    #s_teach.append(a)
 
 picture = world_digits()
 
-picture.init()
+data = picture.getall()
 
-print(picture.sensor)
+for char in data[1]:
+    code = ord(char) - 48
+    binary = list(("{0:b}".format(code)).zfill(6))
+    binary = [float(i) for i in binary]
+    print(char, code, str(binary))
+    s_teach.append(binary)
 
 
+s_in = n.array(data[0])
+s_teach = n.array(s_teach)
 
+
+print( str(s_in.shape)) 
+print( str(s_teach.shape) )
+
+print( str(type(s_in))) 
+print( str(type(s_teach)) )
 
 
 from NeuralNetwork import NeuralNetwork
 
 nn = NeuralNetwork([64,100,6]) 
 
+print(type(s_in[1]))
 
+nn.teach(n.array(s_in), n.array(s_teach) ,0.3,25000)  # Trainiren: 
 
 #nn.teach(s_in, s_teach ,0.3,25000)  # Trainiren: 
-
-
-#for i in [[0, 0], [0, 1], [1, 0], [1,1]]:
-#    print(i,nn.guess(i))
+for i in [3, 7, 9, 14, 25]:
+    print(data[1][i],nn.guess(s_in[i]))
 
 # whoaaaa: sichern von Daten zum laden fürs nächste Mal ;)
 #nn.save('savetest') # erzeugt eine 'savetest.npz' Datei! (alles unchecked!, überschreiben ohne Warnung!)
     
 #nn.load('savetest') # läd eine 'savetest.npz' Datei! (alles unchecked!)
 
-
+print('fertig')
 
 
 
--- PyMLP/XOR.py
+++ PyMLP/XOR.py
@@ -10,6 +10,20 @@ nn = NeuralNetwork([2,6,1]) # Erstelle neues Neurales Netzwerk mit 2 Eingangsneu
 s_in = n.array([[0, 0], [0, 1], [1, 0], [1, 1]]) #Trainingsdaten Input
 s_teach = n.array([[0], [1], [1], [0]])          #Trainingsdaten Output
 
+
+print( str(s_in.shape)) 
+print( str(s_teach.shape) )
+
+print( str(type(s_in))) 
+print( str(type(s_teach)) )
+
+
+print( str(s_in[0].shape)) 
+print( str(s_teach[0].shape) )
+
+print( str(type(s_in[0]))) 
+print( str(type(s_teach[0])) )
+
 nn.teach(s_in, s_teach ,0.3,25000)  # Trainiren: 
 #s_in: Input Daten als numpy-Array
 #s_teach: Output Daten als numpy-Array
@@ -17,8 +31,8 @@ nn.teach(s_in, s_teach ,0.3,25000)  # Trainiren:
 # optional: repeats=10000: Wiederholungen
 
 
-for i in [[0, 0], [0, 1], [1, 0], [1,1]]:
-    print(i,nn.guess(i))
+#for i in [[0, 0], [0, 1], [1, 0], [1,1]]:
+#    print(i,nn.guess(i))
 
 # whoaaaa: sichern von Daten zum laden fürs nächste Mal ;)
 #nn.save('savetest') # erzeugt eine 'savetest.npz' Datei! (alles unchecked!, überschreiben ohne Warnung!)
