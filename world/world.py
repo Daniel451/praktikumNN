348ed6bb17276e361e67cc9a6f4c5bfce2097140
Florian Kock <florian-kock@gmx.de>
2014-11-17 16:14:33 +0100

    init SARSA

---
+++ world/world.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.4
+#-*- coding: utf-8 -*-
+
+import numpy as n
+
+class World:
+    def __init__(self, x,y):
+        world = n.array(x,y)
+        print(world)
+
+
+
+
+
+x = new World(10,20)
