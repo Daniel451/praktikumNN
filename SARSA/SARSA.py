f28033b2de62ecf2e9f8f25b7907db40a875fc44
Daniel Speck <daniel@dspeck.de>
2014-11-24 15:41:39 +0100

    bla

--- SARSA/SARSA.py
+++ SARSA/SARSA.py
@@ -1,4 +1,4 @@
-
+#-*- coding: utf-8 -*-
 
 
 import random
@@ -22,15 +22,17 @@ class world:
 
         self.newRandomStartPosition()
 
-        self.ind = numpy.arange(0, self.size_x * self.size_y)
-
 
     def newRandomStartPosition(self):
+        """
+        calculates a new random start position (x,y) for our robot
+        """
         self.x = random.randint(0, size_x-1)
         self.y = random.randint(0, size_y-1)
 
 
     def doAction(self, action):
+
         # position world redoActionion
         if  numpy.random.random_sample() > 0.0:
             if  action == 0:      #down
@@ -43,15 +45,18 @@ class world:
                 self.y -= 1
             else:
                 print('unknown action')
-                # Ränder von Welt bleiben Ränder! Bewegung bleit stehen!
+                # borders remain, movement stops 
+
         if   self.x < 0:
             self.x = 0
         elif self.x >= self.size_x:
             self.x = self.size_x - 1
+
         if   self.y < 0:
             self.y = 0
         elif self.y >= self.size_y:
             self.y = self.size_y - 1
+
 
     def get_reward(self):
         if  self.x == 2 and self.y == 3:
@@ -59,16 +64,22 @@ class world:
         else:
             return 0.0
             
+
     def get_sensor(self):
         p = numpy.zeros(self.size_x * self.size_y)
         p[self.x * self.size_x + self.y] = 1.0
         return p
     
+
     def get_size_X():
         return self.size_x
         
+
     def get_size_Y():
         return self.size_y
+
+
+
 
 
 def nextAction (S_from, beta):
